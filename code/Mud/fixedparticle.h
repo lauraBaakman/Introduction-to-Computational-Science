77944a192bc3247493d597981af990c0b9a20965
Laura Baakman <laurabaakman@gmail.com>
2015-12-22 14:09:47 +0100

    Class for fixed particle

---
+++ code/Mud/fixedparticle.h
@@ -0,0 +1,12 @@
+#ifndef FIXEDPARTICLE_H
+#define FIXEDPARTICLE_H
+
+#include "particle.h"
+
+class FixedParticle : public Particle
+{
+public:
+    FixedParticle();
+};
+
+#endif // FIXEDPARTICLE_H
