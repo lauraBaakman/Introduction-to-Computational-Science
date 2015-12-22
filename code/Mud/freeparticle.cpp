b7ac73b75319cbd0e48a5d8323c63785dcf4ba03
Laura Baakman <laurabaakman@gmail.com>
2015-12-22 14:10:32 +0100

    Class for free particle

---
+++ code/Mud/freeparticle.cpp
@@ -0,0 +1,7 @@
+#include "freeparticle.h"
+
+FreeParticle::FreeParticle()
+{
+
+}
+
---
+++ code/Mud/freeparticle.h
@@ -0,0 +1,12 @@
+#ifndef FREEPARTICLE_H
+#define FREEPARTICLE_H
+
+#include "particle.h"
+
+class FreeParticle : public Particle
+{
+public:
+    FreeParticle();
+};
+
+#endif // FREEPARTICLE_H
