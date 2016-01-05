dd047ffba49b687bc4c33598eafde48d0afcf5eb
Laura Baakman <laurabaakman@gmail.com>
2016-01-05 10:49:40 +0100

    Made the generator static.

--- code/Mud/spring.cpp
+++ code/Mud/spring.cpp
@@ -1,15 +1,15 @@
 #include "spring.h"
 
 int Spring::nextId = 0;
+std::default_random_engine Spring::generator;
 
-Spring::Spring(Particle *particleA, Particle *particleB,
-               float springConstant, bool broken) :
-    springConstant(springConstant),
+Spring::Spring(Particle *particleA, Particle *particleB, bool broken) :
     broken(broken),
     particleA(particleA),
     particleB(particleB)
 {
     id = nextId++;
+    springConstant = sampleSpringConstant();
 }
 
 Particle *Spring::getParticleA() const
@@ -37,6 +37,22 @@ const Particle *Spring::getOtherParticle(const Particle *particle) const
 void Spring::clear()
 {
     Spring::nextId = 0;
+}
+
+void Spring::setSpringConstantDistributionParameters(float mean, float standardDeviation)
+{
+    //TODO implement
+    qDebug() << "Implement the function setSpringConstantDistributionParameters!";
+    exit(-1);
+}
+
+float Spring::sampleSpringConstant()
+{
+    std::normal_distribution<double> distribution(5.0,2.0);
+
+    float number = distribution(Spring::generator);
+    qDebug() << "Generating a random number: " << number << &endl;
+    return number;
 }
 
 int Spring::getId() const
--- code/Mud/spring.h
+++ code/Mud/spring.h
@@ -1,6 +1,8 @@
 #ifndef SPRING_H
 #define SPRING_H
 
+#include <random>
+
 #include <QDebug>
 #include <QVector3D>
 
@@ -11,8 +13,7 @@ class Particle;
 class Spring
 {
 public:
-    Spring(Particle *particleA = nullptr, Particle *particleB = nullptr,
-           float springConstant = 1.0, bool broken = false);
+    Spring(Particle *particleA = nullptr, Particle *particleB = nullptr, bool broken = false);
 
     friend QDebug operator<<(QDebug stream, const Spring &spring);
     friend QDebug operator<<(QDebug stream, Spring *spring);
@@ -27,16 +28,22 @@ public:
     int getId() const;
     static void clear();
 
+    static void setSpringConstantDistributionParameters(float mean = 0.0, float standardDeviation = 1.0);
+
 private:
     float springConstant;
     bool broken;
-
-    static int nextId;
     int id;
 
     Particle *particleA;
     Particle *particleB;
 
+    static int nextId;
+
+//    static std::normal_distribution<float> distribution;
+    static std::default_random_engine generator;
+
+    float sampleSpringConstant();
 };
 
 #endif // SPRING_H
